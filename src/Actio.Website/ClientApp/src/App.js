7faa2b006282d4c3ee9411d179057b289a1d0add
TUAN TRAN <dictuantran@gmail.com>
2019-06-24 21:58:12 +0700

    add bootstrap to website

--- src/Actio.Website/ClientApp/src/App.js
+++ src/Actio.Website/ClientApp/src/App.js
@@ -1,14 +1,11 @@
-import React from 'react';
-import { Route } from 'react-router';
-import Layout from './components/Layout';
-import Home from './components/Home';
-import Counter from './components/Counter';
-import FetchData from './components/FetchData';
+import React, { Component } from 'react';
 
-export default () => (
-  <Layout>
-    <Route exact path='/' component={Home} />
-    <Route path='/counter' component={Counter} />
-    <Route path='/fetch-data/:startDateIndex?' component={FetchData} />
-  </Layout>
-);
+class App extends Component {
+  render () {
+    return (
+        <h1>Hello Reactjs</h1>
+    )
+  }
+}
+
+export default App
