2b3e45231c197ca4d55fe23e0db0813648a5fe66
TUAN TRAN <dictuantran@gmail.com>
2019-07-07 11:52:14 +0700

    002 - api

--- src/Actio.Website.Product/src/Selector.js
+++ src/Actio.Website.Product/src/Selector.js
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Redirect } 
     from "react-router-dom";
 import { ToggleLink } from "./routing/ToggleLink";
-//import { CustomPrompt } from "./routing/CustomPrompt";
 import { RoutedDisplay } from "./routing/RoutedDisplay";
+import { IsolatedTable } from "./IsolatedTable";
+import { IsolatedEditor } from "./IsolatedEditor";
+import { RequestError } from "./webservice/RequestError";
 
 export class Selector extends Component {
 
@@ -20,12 +22,19 @@ export class Selector extends Component {
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-2">            
+                        <ToggleLink to="/isolated">Isolated Data</ToggleLink>
                         { routes.map(r => <ToggleLink key={ r.url } to={ r.url }>
                                             { r.name }
                                         </ToggleLink>)}
                     </div>
                     <div className="col">
                         <Switch>
+                            <Route path="/isolated" component={ IsolatedTable } 
+                                exact={ true } />
+                            <Route path="/isolated/:mode/:id?" 
+                                component={ IsolatedEditor } />
+                            <Route path="/error/:message" 
+                                component={ RequestError } />
                             { routes.map(r => 
                                <Route key={ r.url } 
                                    path={ `/:datatype(${r.datatype})/:mode?/:id?`}
