90a53cbf8148a6330f66bd85d7ef06024bd73577
TUAN TRAN <dictuantran@gmail.com>
2019-07-12 22:14:06 +0700

    004-build container dashboard

--- src/Actio.Website.Blog/src/App.js
+++ src/Actio.Website.Blog/src/App.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import routes from "./routes";
 
-export default () => <div>
-    Hello Blog
-</div>
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./shards-dashboard/styles/shards-dashboards.1.1.0.min.css";
+
+export default () => (
+    <Router basename={process.env.REACT_APP_BASENAME || ""}>
+        <div>                    
+            {routes.map((route, index) => {
+                return (
+                    "Hello Blog"
+                );
+            })}
+        </div>
+    </Router>
+)
